1791425af2d1b65c6dfa800bd3c59c2e4270a71e
R1fty0 <113545024+R1fty0@users.noreply.github.com>
2023-02-21 11:44:50 -0800

    Commit 1 (2023-02-21)
    - Changed the name: encryptString() -> encodeString()
    
    - Return the encoded String after printing it
    
    - Removed this:
    def __init__(self, character, number, message, key):
            self.character = character
            self.number = number
            self.message = message
            self.key = key
    
    - Wrap back around if the int the char is cast to goes above 126 or below 32  * May be incomplete
    
    - Create another method called encodeData that accepts a list of Strings, encodes all of them, and returns a list of the encoded content

--- Code/encryption.py
+++ Code/encryption.py
@@ -1,26 +1,55 @@
-class FileEncrypter:
-    def __init__(self, character, number, message, key):
-        self.character = character
-        self.number = number
-        self.message = message
-        self.key = key
-        print('File encrypter created!')
+import sys
 
-    def characterCast(self, character):
+
+class FileEncrypter:
+    def __init__(self):
+        print("File encrypter created!")
+
+    def get_char_unicode_val(self, character):
+        """ Returns the Unicode code point for the given character in the ASCII encoding. """
         return ord(character)
 
     def convertBackToChar(self, number):
         return chr(number)
 
-    def encryptString(self, message, key):
-        ''' Change the value of the given String to one shifted using a Cesarean cipher '''
+    def encode_string(self, message: str, cipher_key: int) -> str:
+        """ Encodes a given string using a Cesarean cipher and returns the encoded string. """
         encryptedString = ''
+        try:
+            messageList = list(message)
 
-        messageList = list(message)
+            for character in messageList:
+                char_unicode = ord(character)  # Gets the character's unicode code point
 
-        for character in messageList:
-            intVal = ord(character)
-            newChar = chr(intVal + key)
-            encryptedString += newChar
+                # Checks to see if character's unicode is not a unicode representing a letter
+                if char_unicode > 126:
+                    char_unicode -= 95
+                elif char_unicode < 32:
+                    char_unicode += 95
+                else:
+                    # encrypts the character based on the cipher's key
+                    new_encoded_char = chr(char_unicode + cipher_key)
+                    encryptedString += new_encoded_char
 
-        print("Encrypted message: " + encryptedString)
+            print(f"Encrypted message: {encryptedString}")  # prints the encoded string
+            return encryptedString  # returns the encoded string
+
+        except TypeError:
+            print("An error occurred.")
+
+
+    def encode_to_file(self, new_file_name: str, data):
+        """ Data given will be encoded as well as written to a custom file. """
+        pass
+
+    def encode_list_of_data(self, data: list, cipher_key) -> list:
+        """ Accepts a list of Strings, encodes all of them, and returns a list of the encoded content. """
+        encoded_content = list()
+        try:
+            for string in data:  # Encodes each string in list of data
+                encoded_data = self.encode_string(string, cipher_key)
+                encoded_content.append(encoded_data)
+
+            return encoded_content  # Returns list containing encoded data
+        except TypeError:
+            print("The data or cipher key you inputted is invalid. Please try again and enter a proper value for both attributes.")
--- Code/tester.py
+++ Code/tester.py
@@ -1,4 +1,11 @@
+from encryption import FileEncrypter
 from reader import FileRead
 from writer import FileWrite
-reader = FileRead()
-writer = FileWrite()
+
+encoder = FileEncrypter()
+
+encoder.encode_string("Bruh", 1) # CSVI (+1)
+print("----------------------------------")
+encoder.encode_string("Bruh", -1) # AQTG (-1)
+
+
